79207a8ccc392c3709aee5be44daf841030d93b0
Matthew Shirley <mattshirley@hey.com>
2021-08-20 00:01:16 -0700

    chore: use install executable to obtain libpact (#3)
    * chore: use install executable to obtain libpact
    
    * chore: bump version
    
    * chore: update ci to handle installation
    
    * chore: use download path env for ci
    
    * chore: set target test coverage percentage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.0
+
+- chore: use install executable to obtain libpact
+
 ## 0.2.0
 
 - feat: implement dsl for creating pact matchers
--- lib/src/ffi/dylib.dart
+++ lib/src/ffi/dylib.dart
@@ -2,20 +2,29 @@ import 'dart:io';
 import 'dart:ffi';
 import 'package:path/path.dart' as path;
 
-String getlibraryName() {
-  if (Platform.isLinux) {
-    return 'libpact_ffi-linux-x86_64.so';
+String getLibDirectory() {
+  final env = Platform.environment['PACT_DART_LIB_DOWNLOAD_PATH'];
+  if (env != null && env.isNotEmpty) {
+    return env;
   }
-  if (Platform.isMacOS) return 'libpact_ffi-osx-x86_64.dylib';
-  if (Platform.isWindows) return 'pact_ffi-windows-x86_64.dll';
+
+  return '/usr/local/lib';
+}
+
+String getLibName() {
+  if (Platform.isLinux) {
+    return 'libpact_ffi.so';
+  }
+  if (Platform.isMacOS) return 'libpact_ffi.dylib';
+  if (Platform.isWindows) return 'pact_ffi.dll';
 
   throw Exception('Package does not support the current platform');
 }
 
 DynamicLibrary openLibrary() {
-  final libraryName = getlibraryName();
-  final libraryPath =
-      path.join(Directory.current.path, 'dependencies', libraryName);
+  final name = getLibName();
+  final libDir = getLibDirectory();
+  final libPath = path.join(libDir, name);
 
-  return DynamicLibrary.open(libraryPath);
+  return DynamicLibrary.open(libPath);
 }
