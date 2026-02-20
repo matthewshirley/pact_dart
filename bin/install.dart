5ae3b673d6178d5ba08ad2dc136f83a4cfa6f85a
Matthew Shirley <mattshirley@hey.com>
2026-02-20 15:40:32 -0800

    update install script to include headers, pass version as arg

--- bin/install.dart
+++ bin/install.dart
@@ -7,7 +7,9 @@ library install;
 
 import 'dart:ffi';
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:args/args.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 
@@ -77,8 +79,9 @@ String getArchitecture() {
   }
 }
 
-Uri generateDependencyLink(String name, String version, String fileType) {
+Uri getLibraryUri(String name, String version) {
   final architecture = getArchitecture();
+  final fileType = getPlatformFileType();
   final operatingSystem = Platform.operatingSystem;
 
   final path =
@@ -87,64 +90,94 @@ Uri generateDependencyLink(String name, String version, String fileType) {
   return Uri(scheme: 'https', host: 'github.com', path: path);
 }
 
-Future<void> downloadDependency(String name, String version) async {
-  final fileType = getPlatformFileType();
-  final dependencyLink = generateDependencyLink(name, version, fileType);
+Uri getHeadersUri(String version) {
+  final path =
+      '/pact-foundation/pact-reference/releases/download/libpact_ffi-v$version/pact.h';
 
-  print('🛜 Downloading from: ${dependencyLink.toString()}');
-
-  try {
-    final res = await http.get(dependencyLink);
-    if (res.statusCode != 200) {
-      throw Exception('Download failed with status code ${res.statusCode}');
-    }
-
-    final library = GZipCodec().decode(res.bodyBytes);
-
-    final libDir = getLibDirectory();
-    final libDirFile = Directory(libDir);
-    await libDirFile.create(recursive: true);
-
-    final libPath = path.join(libDir, '$name.$fileType');
-    print('💾 Installing to: $libPath');
-
-    try {
-      await File(libPath).writeAsBytes(library, flush: true);
-      print('✅ Successfully installed $name v$version.');
-    } catch (e) {
-      throw Exception(
-          'Unable to write to $libPath. Try running with sudo or specify a different directory with PACT_DART_LIB_DOWNLOAD_PATH environment variable.');
-    }
-  } catch (e) {
-    print('😢 An error occurred during installation: $e');
-    rethrow;
-  }
+  return Uri(scheme: 'https', host: 'github.com', path: path);
 }
 
-void main() async {
-  final dependencyName = Platform.isWindows ? 'pact_ffi' : 'libpact_ffi';
-  const dependencyVersion = '0.4.27';
-  final libDir = getLibDirectory();
+List<int> extractGZipBytes(Uint8List bytes) {
+  return GZipCodec().decode(bytes);
+}
 
-  print('🚀 Pact Dart Installation');
-  print('======================');
+Future<Uint8List> download(Uri uri) async {
+  final res = await http.get(uri);
+  if (res.statusCode != 200) {
+    throw Exception(
+        'Resource returned a non-success status code (got ${res.statusCode})');
+  }
+
+  return res.bodyBytes;
+}
+
+Future<void> install(
+    String directory, String name, String fileType, List<int> bytes) async {
+  final libDirFile = Directory(directory);
+  await libDirFile.create(recursive: true);
+
+  final libPath = path.join(directory, '$name.$fileType');
+  await File(libPath).writeAsBytes(bytes, flush: true);
+}
+
+void main(List<String> args) async {
+  print('Running Pact FFI Install...');
+  print('====================== \n');
   print(
-      'This script will install the $dependencyName library required by Pact Dart:');
-  print('- Library: $dependencyName v$dependencyVersion');
-  print('- Platform: ${Platform.operatingSystem} (${getArchitecture()})');
-  print('- Install directory: $libDir');
-  print('');
+      'This script installs "Pact FFI" from the Pact Foundation. This is a shared library');
+  print('that Pact Dart creates bindings to. \n');
   print(
-      'Note: You may need admin privileges to write to the installation directory.');
+      'Note: You may need `sudo` privileges to write to the installation directory.');
   print(
-      'If installation fails, you can specify an alternative directory using');
-  print('the PACT_DART_LIB_DOWNLOAD_PATH environment variable.');
-  print('');
+      'Alternatively, set `PACT_DART_LIB_DOWNLOAD_PATH` to install to a user path.\n');
+  print('====================== \n');
+
+  final parser = ArgParser()
+    ..addOption('version',
+        abbr: 'v', defaultsTo: '0.4.27', help: 'Pact FFI version to install')
+    ..addFlag("include-headers",
+        abbr: "i",
+        defaultsTo: false,
+        help: "Download Pact FFI headers for development");
+
+  final options = parser.parse(args);
+  final version = options['version'] as String;
+  final includeHeaders = options['include-headers'];
+
+  List<int> libBytes;
+  final libName = Platform.isWindows ? 'pact_ffi' : 'libpact_ffi';
+  final libUri = getLibraryUri(libName, version);
+  print("Getting $libName from $libUri...");
 
   try {
-    await downloadDependency(dependencyName, dependencyVersion);
+    final bytes = await download(libUri);
+    libBytes = extractGZipBytes(bytes);
   } catch (e) {
-    print('\nSomething went wrong: $e');
+    print("Unable to download $libName: $e.");
     exit(1);
+  }
+
+  final libDir = getLibDirectory();
+  final libFileType = getPlatformFileType();
+  print("Saving $libName to $libDir...");
+
+  try {
+    await install(libDir, libName, libFileType, libBytes);
+  } catch (e) {
+    print('Unable to save library to disk: $e');
+    exit(1);
+  }
+
+  if (includeHeaders == true) {
+    final headerUri = getHeadersUri(version);
+    print("Getting `pact.h` from $headerUri");
+
+    try {
+      final bytes = await download(headerUri);
+      await install(".", "pact", "h", bytes);
+    } catch (e) {
+      print("Unable to download pact.h: $e.");
+      exit(1);
+    }
   }
 }
